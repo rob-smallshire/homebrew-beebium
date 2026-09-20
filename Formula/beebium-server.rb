359ea3cbd4796b415df65c207a8fdf8e7ecafaf2
Beebium CI <robert@smallshire.org.uk>
2026-09-20 21:43:56 +0000

    beebium-server 0.2.0

--- Formula/beebium-server.rb
+++ Formula/beebium-server.rb
@@ -12,8 +12,8 @@
 class BeebiumServer < Formula
   desc "Headless BBC Micro emulator servers for Model B, B+, B+ 128K and ROM/RAM"
   homepage "https://github.com/rob-smallshire/beebium"
-  url "https://github.com/rob-smallshire/beebium/archive/refs/tags/v0.1.17.tar.gz"
-  sha256 "0cc2493b803e50482b64e7844682e60561aa845eab7f063e3560ee51b1c73949"
+  url "https://github.com/rob-smallshire/beebium/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "4e6a6d9065c379e810e96412f71f95b2b90f1c6c697f364f014b27615d39f75b"
   license "GPL-3.0-or-later"
   head "https://github.com/rob-smallshire/beebium.git", branch: "master"
 
